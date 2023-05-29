645500fbfaa3db22e1334660f2aff970285bad2e
Aniket Paul <16riju@gmail.com>
2023-05-30 02:39:56 +0530

    call back function

--- src/Components/ChildA.js
+++ src/Components/ChildA.js
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ChildB from './ChildB';
 
 const ChildA = ({ name, onNameSubmit, onVerification }) => {
-    const [receivedName,setReceivedName] = useState("");
   const handleSubmit = () => {
-    console.log(name)
-    setReceivedName(name)
     onNameSubmit(name);
-    
   };
 
-  const handleInputChange=(e)=>
-  {
-    onNameSubmit(e.target.value);
-  }
   return (
     <div>
-      <input type="text" value={name} onChange={handleInputChange} />
-      <button onClick={handleSubmit}>Submit</button>
-      <ChildB name={receivedName} onVerification={onVerification} />
+      
+      <ChildB name={name} onNameSubmit={handleSubmit} onVerification={onVerification} />
     </div>
   );
 };
--- src/Components/GetName.js
+++ src/Components/GetName.js
@@ -4,18 +4,25 @@ import ChildA from './ChildA';
 const GetName = () => {
   const [name, setName] = useState('');
   const [verified, setVerified] = useState('');
-
+  const [received,setReceivedName] = useState('');
   const handleNameSubmit = (data) => {
     setName(data);
   };
 
   const handleVerification = (isChecked) => {
-    setVerified(isChecked ? 'Verified by Component A' : 'Something went wrong');
+    setVerified(isChecked ? 'Verified' : 'Something went wrong');
   };
-
+  const handleSubmit = () => {
+    setReceivedName(name);
+  };
+  const handleInputChange=(e)=>{
+    setName(e.target.value)
+  }
   return (
     <div>
-      <ChildA name={name} onNameSubmit={handleNameSubmit} onVerification={handleVerification} />
+      <input type="text" value={name} onChange={handleInputChange} />
+      <button onClick={handleSubmit}>Submit</button>
+      <ChildA name={received} onNameSubmit={handleNameSubmit} onVerification={handleVerification} />
       {verified && <p>Verification: {verified}</p>}
     </div>
   );
