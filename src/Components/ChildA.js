5b27dce2cd55ad67ed84650bf53cb98ae67bf8de
Aniket Paul <16riju@gmail.com>
2023-05-30 02:28:45 +0530

    Prop drilling

--- src/Components/ChildA.js
+++ src/Components/ChildA.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ChildB from './ChildB';
 
+<<<<<<< Updated upstream
 const ChildA = ({ name }) => {
   return <ChildB name={name} />;
+=======
+const ChildA = ({ name, onVerification }) => {
+   
+  
+  return (
+    <div>
+    
+       <ChildB name={name} onVerification={onVerification} />
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default ChildA;
--- src/Components/GetName.js
+++ src/Components/GetName.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import ChildA from './ChildA';
 
 const GetName = () => {
+<<<<<<< Updated upstream
     const [name, setName] = useState('');
 
     const handleNameChange = (event) => {
@@ -18,5 +19,37 @@ const GetName = () => {
         </>
     )
 }
+=======
+  const [name, setName] = useState('');
+  const [verified, setVerified] = useState('');
+  const [receivedName,setReceivedName] = useState("");
+  const handleNameSubmit = (data) => {
+    setName(data);
+  };
+
+  const handleVerification = (isChecked) => {
+    setVerified(isChecked ? 'Verified by Component A' : 'Something went wrong');
+  };
+  const handleSubmit = () => {
+    console.log(name)
+   
+    setReceivedName(name);
+    
+  };
+
+  const handleInputChange=(e)=>
+  {
+    setName(e.target.value);
+  }
+  return (
+    <div>
+      <input type="text" value={name} onChange={handleInputChange} />
+      <button onClick={handleSubmit}>Submit</button>
+       <ChildA name={receivedName} onNameSubmit={handleNameSubmit} onVerification={handleVerification} />
+      {verified && <p>Verification: {verified}</p>} 
+    </div>
+  );
+};
+>>>>>>> Stashed changes
 
 export default GetName
